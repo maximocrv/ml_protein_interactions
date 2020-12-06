7d44fd9156fab97e885f4acffd714fce5ecf02c8
Maximo Cravero <maximo@geronimo.ai>
2020-12-06 21:57:44 +0100

    tweak siamese

--- scripts/siamese_network.py
+++ scripts/siamese_network.py
@@ -1,18 +1,17 @@
+import math
+import multiprocessing as mp
 import os
 import re
-import math
 from pathlib import Path
-import multiprocessing as mp
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
 import torch
 import torch.nn as nn
+from constants import wt_features_path, mut_features_path, skempi_csv, R
+from scipy.stats import pearsonr
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
-from scipy.stats import pearsonr
 
 # check if we are in a conda virtual env
 try:
@@ -25,41 +24,34 @@ except KeyError:
 def standardize(arr):
     return (arr - np.mean(arr)) / np.std(arr)
 
-DATA_PATH='../data/'
-SKEMPI_CSV=DATA_PATH + 'skempi_v2_cleaned.csv'
-WT_FEATURE_PATH=DATA_PATH + 'openmm/'
-MUT_FEATURE_PATH=DATA_PATH + 'openmm_mutated/'
-
-R = (8.314/4184)  # kcal mol^-1 K^-1
-
 
 def siamese_preprocessing(pandas_row):
     name_wt = pandas_row[1].iloc[0]
     name_mut = pandas_row[1].iloc[0] + '_' + pandas_row[1].iloc[2].replace(',', '_')
 
-    if not Path(MUT_FEATURE_PATH + 'D_mat/' + name_mut + '.npy').exists():
+    if not Path(mut_features_path + 'D_mat/' + name_mut + '.npy').exists():
         print(f'ERROR: {name_mut} does not exist.', '\n')
         return None
-    if not Path(WT_FEATURE_PATH + 'D_mat/' + name_wt + '.npy').exists():
+    if not Path(wt_features_path + 'D_mat/' + name_wt + '.npy').exists():
         print(f'ERROR: {name_wt} does not exist.', '\n')
         return None
 
-    d_mat_wt = standardize(np.load(WT_FEATURE_PATH + 'D_mat/' + name_wt + '.npy'))
-    u_lj_wt = standardize(np.load(WT_FEATURE_PATH + 'U_LJ/' + name_wt + '.npy'))
-    u_el_wt = standardize(np.load(WT_FEATURE_PATH + 'U_el/' + name_wt + '.npy'))
+    d_mat_wt = standardize(np.load(wt_features_path + 'D_mat/' + name_wt + '.npy'))
+    u_lj_wt = standardize(np.load(wt_features_path + 'U_LJ/' + name_wt + '.npy'))
+    u_el_wt = standardize(np.load(wt_features_path + 'U_el/' + name_wt + '.npy'))
 
     wt_arr = np.stack([d_mat_wt, u_lj_wt, u_el_wt])
 
-    d_mat_mut = standardize(np.load(MUT_FEATURE_PATH + 'D_mat/' + name_mut + '.npy'))
-    u_lj_mut = standardize(np.load(MUT_FEATURE_PATH + 'U_LJ/' + name_mut + '.npy'))
-    u_el_mut = standardize(np.load(MUT_FEATURE_PATH + 'U_el/' + name_mut + '.npy'))
+    d_mat_mut = standardize(np.load(mut_features_path + 'D_mat/' + name_mut + '.npy'))
+    u_lj_mut = standardize(np.load(mut_features_path + 'U_LJ/' + name_mut + '.npy'))
+    u_el_mut = standardize(np.load(mut_features_path + 'U_el/' + name_mut + '.npy'))
 
     mut_arr = np.stack([d_mat_mut, u_lj_mut, u_el_mut])
 
     # calculate DDG
     A_wt = pandas_row[1]['Affinity_wt_parsed']
     A_mut = pandas_row[1]['Affinity_mut_parsed']
-    
+
     # print(pandas_row[1]['Temperature'])
     temp = float(re.match("[0-9]*", pandas_row[1]['Temperature'])[0])
     if math.isnan(temp):
@@ -75,68 +67,71 @@ def siamese_preprocessing(pandas_row):
     return np.stack([wt_arr, mut_arr]), DDG
 
 
-def gen_loaders(x, y, batch_size):    
+def gen_loaders(x, y, batch_size):
     x_tensor, y_tensor = torch.from_numpy(x), torch.from_numpy(y)
-    
+
     data = TensorDataset(x_tensor, y_tensor)
-   
+
     loader = DataLoader(dataset=data, batch_size=batch_size, shuffle=False)
 
     return loader
+
 
 class HydraNet(nn.Module):
     def __init__(self):
         super().__init__()
         # feature map output: [(W - K + 2P) / S] + 1
         # include batch norm, dropout (remember model.train() and model.eval() !!!)
-        self.cnn1 = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, stride=2), #output: (256-3)/2 + 1 =
-                                 nn.ReLU(),
-                                 nn.MaxPool2d(3, stride=2),
-                                 nn.BatchNorm2d(8),   
+        self.cnn1 = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, stride=2),
+                                  # output: (256-3)/2 + 1 =
+                                  nn.ReLU(),
+                                  nn.MaxPool2d(3, stride=2),
+                                  nn.BatchNorm2d(8),
 
-                                 nn.Conv2d(in_channels=8, out_channels=64, kernel_size=3, stride=2),
-                                 nn.ReLU(),
-                                 nn.MaxPool2d(3, stride=2),
-                                 nn.BatchNorm2d(64),
+                                  nn.Conv2d(in_channels=8, out_channels=64, kernel_size=3, stride=2),
+                                  nn.ReLU(),
+                                  nn.MaxPool2d(3, stride=2),
+                                  nn.BatchNorm2d(64),
 
-                                 nn.Conv2d(in_channels=64, out_channels=512, kernel_size=3),
-                                 nn.ReLU(),
-                                 nn.MaxPool2d(3, stride=2),
-                                 nn.BatchNorm2d(512),
+                                  nn.Conv2d(in_channels=64, out_channels=512, kernel_size=3),
+                                  nn.ReLU(),
+                                  nn.MaxPool2d(3, stride=2),
+                                  nn.BatchNorm2d(512),
 
-                                 nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2),
-                                 nn.ReLU(),
-                                 nn.MaxPool2d(2)
-                                )
+                                  nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2),
+                                  nn.ReLU(),
+                                  nn.MaxPool2d(2)
+                                  )
 
-        self.cnn2 = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, stride=2), #output: (256-3)/2 + 1 =
-                                 nn.ReLU(),
-                                 nn.MaxPool2d(3, stride=2),
-                                 nn.BatchNorm2d(8),
+        self.cnn2 = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, stride=2),
+                                  # output: (256-3)/2 + 1 =
+                                  nn.ReLU(),
+                                  nn.MaxPool2d(3, stride=2),
+                                  nn.BatchNorm2d(8),
 
-                                 nn.Conv2d(in_channels=8, out_channels=64, kernel_size=3, stride=2),
-                                 nn.ReLU(),
-                                 nn.MaxPool2d(3, stride=2),
-                                 nn.BatchNorm2d(64),
-                                 
-                                 nn.Conv2d(in_channels=64, out_channels=512, kernel_size=3),
-                                 nn.ReLU(),
-                                 nn.MaxPool2d(3, stride=2),
-                                 nn.BatchNorm2d(512),
-                                 
-                                 nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2),
-                                 nn.ReLU(),
-                                 nn.MaxPool2d(2)
-                                )
+                                  nn.Conv2d(in_channels=8, out_channels=64, kernel_size=3, stride=2),
+                                  nn.ReLU(),
+                                  nn.MaxPool2d(3, stride=2),
+                                  nn.BatchNorm2d(64),
+
+                                  nn.Conv2d(in_channels=64, out_channels=512, kernel_size=3),
+                                  nn.ReLU(),
+                                  nn.MaxPool2d(3, stride=2),
+                                  nn.BatchNorm2d(512),
+
+                                  nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2),
+                                  nn.ReLU(),
+                                  nn.MaxPool2d(2)
+                                  )
 
         # each output of self.cnn will have dimension 1024, so when concatenated we have 2048
-        self.fc = nn.Sequential(#nn.Linear(2048, 512),
-                                #nn.ReLU(),
-                                nn.Dropout2d(p=0.3),
-                                nn.Linear(2*512, 64),
-                                nn.ReLU(),
-                                nn.Dropout2d(p=0.3),
-                                nn.Linear(64, 1))
+        self.fc = nn.Sequential(  # nn.Linear(2048, 512),
+            # nn.ReLU(),
+            nn.Dropout2d(p=0.3),
+            nn.Linear(2 * 512, 64),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.3),
+            nn.Linear(64, 1))
 
     def forward(self, x1):
         output1 = self.cnn1(x1[:, 0])
@@ -155,7 +150,7 @@ def train(_model, _criterion, dataset_train, dataset_test, _optimizer, n_epochs)
     for epoch in range(n_epochs):
         # Train an epoch
         _model.train()
-        train_losses=[]
+        train_losses = []
         for batch_x, batch_y in dataset_train:
             batch_x, batch_y = batch_x.to(device), batch_y.to(device)
 
@@ -171,7 +166,7 @@ def train(_model, _criterion, dataset_train, dataset_test, _optimizer, n_epochs)
             # Update the parameters of the model with a gradient step
             _optimizer.step()
 
-        train_loss=np.mean(train_losses)
+        train_loss = np.mean(train_losses)
 
         # Test the quality on the test set
         _model.eval()
@@ -184,11 +179,12 @@ def train(_model, _criterion, dataset_train, dataset_test, _optimizer, n_epochs)
             prediction = _model(batch_x)
             mse_test.append(_criterion(prediction, batch_y).item())
             # ^^ could be source of memory leak
-        print("Epoch {} | Train loss: {:.5f} Test loss: {:.5f}".format(epoch, train_loss, sum(mse_test)/len(mse_test)))
+        print(
+            "Epoch {} | Train loss: {:.5f} Test loss: {:.5f}".format(epoch, train_loss, sum(mse_test) / len(mse_test)))
 
 
 if __name__ == '__main__':
-    df = pd.read_csv(SKEMPI_CSV, sep=';')
+    df = pd.read_csv(skempi_csv, sep=';')
     # df = df.iloc[:4096, :]
 
     # filter duplicated
@@ -213,17 +209,17 @@ if __name__ == '__main__':
     print(f'{n_non_existant} PDBs do not have features.')
 
     input_arr = np.array(input_list).astype(np.float32)
-    target_arr = np.array(target_list).astype(np.float32)[...,np.newaxis]
-    
+    target_arr = np.array(target_list).astype(np.float32)[..., np.newaxis]
+
     x_tr, x_te, y_tr, y_te = train_test_split(input_arr, target_arr, test_size=0.2, random_state=42)
-    #x_tr, x_val, y_tr, y_val = train_test_split(x_tr, y_tr, test_size=0.25, random_state=42)
-   
+    # x_tr, x_val, y_tr, y_val = train_test_split(x_tr, y_tr, test_size=0.25, random_state=42)
+
     train_data = gen_loaders(x_tr, y_tr, 16)
-    #val_data = gen_loaders(x_val, y_val, 16)
+    # val_data = gen_loaders(x_val, y_val, 16)
     test_data = gen_loaders(x_te, y_te, 16)
 
-    #model = HydraNet()
-    num_epochs = 100 
+    # model = HydraNet()
+    num_epochs = 100
     learning_rate = 1e-3
 
     # If a GPU is available
